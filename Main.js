92c4d9a134cfe81f95cef7e5803c270af0efc231
JonasAtzenhofer <jomasatze@gmail.com>
2023-02-11 19:17:10 +0100

    Canvas Versuch

--- Main.js
+++ Main.js
@@ -1,5 +1,23 @@
 var Eia2Endabgabe;
 (function (Eia2Endabgabe) {
     window.addEventListener("load", handleLoad);
+    function handleLoad(_event) {
+        let canvas = document.querySelector("canvas");
+        if (!canvas)
+            return;
+        Eia2Endabgabe.crc2 = canvas.getContext("2d");
+        canvas.addEventListener("click", drawCircle);
+        function drawCircle(_event) {
+            console.log("click");
+            let x = _event.clientX - canvas.getBoundingClientRect().left;
+            let y = _event.clientY - canvas.getBoundingClientRect().top;
+            console.log("x: " + x, "y: " + y);
+            Eia2Endabgabe.crc2.beginPath();
+            Eia2Endabgabe.crc2.arc(x, y, 30, 0, 2 * Math.PI);
+            Eia2Endabgabe.crc2.fillStyle = "red";
+            Eia2Endabgabe.crc2.fill();
+            Eia2Endabgabe.crc2.closePath();
+        }
+    }
 })(Eia2Endabgabe || (Eia2Endabgabe = {}));
 //# sourceMappingURL=Main.js.map
--- Main.ts
+++ Main.ts
@@ -3,8 +3,28 @@ namespace Eia2Endabgabe {
     window.addEventListener("load", handleLoad);
 
     export let crc2: CanvasRenderingContext2D;
-    
 
-    
 
-}
+
+
+    function handleLoad(_event: Event): void {
+        let canvas: HTMLCanvasElement | null = <HTMLCanvasElement>document.querySelector("canvas");
+        if (!canvas)
+            return;
+        crc2 = <CanvasRenderingContext2D>canvas.getContext("2d");
+        canvas.addEventListener("click", drawCircle);
+    
+        function drawCircle(_event: MouseEvent): void {
+            console.log("click");
+            let x: number = _event.clientX - canvas.getBoundingClientRect().left;
+            let y: number = _event.clientY - canvas.getBoundingClientRect().top;
+            console.log("x: " + x, "y: " + y);
+    
+            crc2.beginPath();
+            crc2.arc(x, y, 30, 0, 2 * Math.PI);
+            crc2.fillStyle = "red";
+            crc2.fill();
+            crc2.closePath();
+        }
+    }
+}    
